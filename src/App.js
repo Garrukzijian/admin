61c31116d80e5d60c7426cddda4e0e54cb9cbc57
Zijian Zhou <86515498+Garrukzijian@users.noreply.github.com>
2022-09-28 10:43:59 +0700

    Update the contract address

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import contract from './contracts/abi.json';
 import {ethers} from 'ethers';
 
-const contractAddress = "0x3AF900DA1a50203EE7FEFe12ebcDc19432cb4005";
+const contractAddress = "0xa00312095A9019F1A5E12401701B21037D736290";
 const abi = contract;
 
 function App() {
